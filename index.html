fdf5fecc6c500ed77ab94e76ba52dd5f019040fb
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2025-08-02 21:01:22 +0530

    Add files via upload

---
+++ index.html
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css?v=<?= filemtime('style.css')?>">
+	<script type="text/javascript"  defer src="java.js?v=<?=filemtime('java.js') ?>"></script>
+	<title>test</title>
+
+</head>
+<body >
+
+<div id="body">
+<div id="img" class="img" style="background-size:cover;">
+</div>
+</div>
+
+	<div id="menu_content"  class="menu_content">
+	<a href="/login"><span></span>SignIn</a>
+	<a href="/register"><span></span>SignUp</a>
+	</div> 
+
+<div  class="content">
+
+<div  id="opener" class="opener"></div>
+<div class="slogan">
+	<div class="text" id="content">
+      <h2>NO MORE HIDDEN TALENTS!  </h2> 
+      <h3>SHOW OFF YOUR PROJECTS AND INSPIRE OTHERS</h3>
+     
+      <a href="#">Explore</a>
+    </div>
+	
+	</div>
+
+</div>
+
+</body>
+</html>
---
+++ login/java.js
@@ -0,0 +1,41 @@
+let img_selector = [
+  "https://images.unsplash.com/photo-1610563166150-b34df4f3bcd6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+  "https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80"
+];
+let i = 0;
+
+let img_element = document.getElementById("img");
+let body_element=document.getElementById("body");
+let login_form=document.getElementById('login_form');
+let query=window.location.search;
+let params=new URLSearchParams(query);
+let re=params.get('re')
+let timer = setInterval(() => {
+  if (img_element.style.opacity == 1 ) {
+    img_element.style.opacity = 0;
+    body_element.style.background="url('"+img_selector[i]+"')";
+ 
+  } else if(img_element.style.opacity == 0){
+
+    img_element.style.background = "url('"+img_selector[i]+"')";
+    img_element.style.opacity = 1;
+
+    if (i === 1) {
+      i = 0;
+    } else {
+      i++;
+    }
+  }
+
+    body_element.style.backgroundRepeat="no-repeat";
+body_element.style.backgroundSize="cover";
+  img_element.style.backgroundRepeat="no-repeat";
+img_element.style.backgroundSize="cover";
+img_element.style.backgroundPosition="centre"
+}, 5000);
+let login_open=setInterval(()=>{
+login_form.style.opacity=1;
+
+},400)
+
+
